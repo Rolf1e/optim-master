bd4da396163e15d2b40965567429b112432a1f88
Rolf1e <tigran.slama@laposte.net>
2020-09-25 13:22:31 +0200

    build change

--- tp1/build.sh
+++ tp1/build.sh
@@ -2,6 +2,7 @@ echo "Building project"
 
 cargo build --release
 cp target/release/libknapsack.so knapsack
+cp target/release/tp1 .
 mv knapsack/libknapsack.so knapsack/knapsack.so
 
 echo "Project builded"
--- tp1/src/main.rs
+++ tp1/src/main.rs
@@ -6,6 +6,7 @@ pub mod resolve;
 
 fn main() {
     let number_execution = 100000;
+    println!("Execute {} times", number_execution);
     let file_content = parsing::create_knapsack_from_file("data1000.txt");
     let res = resolve::random_execution(&file_content.0, number_execution, file_content.1, file_content.2);
     println!("Profit: {}\nWeight: {}\nAttempt: {}\nSolution: {:?}", res.0, res.2, res.3, res.1);
